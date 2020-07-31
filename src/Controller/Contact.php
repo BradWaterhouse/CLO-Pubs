86b0662648d1a377ae276963f37ac5333fb0634f
brad-waterhouse <bradley.waterhouse@bunches.co.uk>
2020-07-31 10:21:56 +0100

    adds controller and usees encore

---
+++ src/Controller/Contact.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class Contact extends AbstractController
+{
+    /**
+     * @Route("/contact-us", name="contact", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        return $this->render('pages/contact.html.twig');
+    }
+}
