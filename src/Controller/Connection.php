7bcf3665fd69c17ffb44ec18178459cdd07ee148
Brad Waterhouse <brad.waterhouse97@gmail.com>
2020-09-07 22:25:11 +0100

    adds missing import

--- src/Controller/Connection.php
+++ src/Controller/Connection.php
@@ -8,6 +8,7 @@ use Doctrine\DBAL\Driver\Connection as DoctrineConnection;
 use PDO;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class Connection
 {
